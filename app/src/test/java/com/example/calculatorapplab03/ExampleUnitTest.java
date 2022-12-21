dbb5ecb071784b96e25a81fbdb1dc07728e73e51
TomiArotiba <ob.arotiba@gmail.com>
2022-12-21 13:54:23 +0200

    Performed Unit Tests

--- app/src/test/java/com/example/calculatorapplab03/ExampleUnitTest.java
+++ app/src/test/java/com/example/calculatorapplab03/ExampleUnitTest.java
@@ -1,5 +1,6 @@
 package com.example.calculatorapplab03;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -11,7 +12,55 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void Test01GetAddition(){
+        String oldNumber = "3";
+        String newNumber = "2";
+
+        double actualResult = Double.parseDouble(oldNumber) + Double.parseDouble(newNumber);
+        double expectedResult = 5.0;
+        Assert.assertEquals(expectedResult, actualResult,0.0001);
+    }
+    @Test
+    public void Test02GetSubtraction(){
+        String oldNumber = "3";
+        String newNumber = "2";
+
+        double actualResult = Double.parseDouble(oldNumber) - Double.parseDouble(newNumber);
+        double expectedResult = 1.0;
+        Assert.assertEquals(expectedResult, actualResult,0.0001);
+    }
+    @Test
+    public void Test03GetDivision(){
+        String oldNumber = "3";
+        String newNumber = "2";
+
+        double actualResult = Double.parseDouble(oldNumber) / Double.parseDouble(newNumber);
+        double expectedResult = 1.5;
+        Assert.assertEquals(expectedResult, actualResult,0.0001);
+    }
+    @Test
+    public void Test04GetMultiplication(){
+        String oldNumber = "3";
+        String newNumber = "2";
+
+        double actualResult = Double.parseDouble(oldNumber) / Double.parseDouble(newNumber);
+        double expectedResult = 6.0;
+        Assert.assertEquals(expectedResult, actualResult,0.0001);
+    }
+    @Test
+    public void Test05GetSquareRoot1(){
+        String oldNumber = "5";
+
+        double actualResult = Math.sqrt(Double.parseDouble(oldNumber));
+        double expectedResult = 2.236;
+        Assert.assertEquals(expectedResult, actualResult,0.0001);
+    }
+    @Test
+    public void Test06GetSquareRoot2(){
+        String oldNumber = "49";
+
+        double actualResult = Math.sqrt(Double.parseDouble(oldNumber));
+        double expectedResult = 7.0;
+        Assert.assertEquals(expectedResult, actualResult,0.0001);
     }
 }
